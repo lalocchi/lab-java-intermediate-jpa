3c7e756fdf23a10efc23c4338158b798c9e9828d
Lala Aliyeva <lala66aliyeva@gmail.com>
2026-04-03 17:02:10 +0400

    Lab 3.04 (LAST vers)

--- lab4_unit3/src/main/java/com/example/lab4_unit3/entity/Task.java
+++ lab4_unit3/src/main/java/com/example/lab4_unit3/entity/Task.java
@@ -31,4 +31,36 @@ public class Task {
 
     private boolean status;
 
+    //getter setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }
